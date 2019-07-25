1bc4321843d5903f565fb754699d200a6718066c
Rajat Pratap Singh <rajasin@microsoft.com>
2019-07-25 11:33:37 +0530

    restructring

--- README.md
+++
@@ -1,2 +0,0 @@
-# RedSkull
-This Is the Repo for Redskull Javascript library. A library to safeguard Web Applications Servers.
---
+++ src/js/RedSkull.js
@@ -0,0 +1,32 @@
+ import CircuitBreaker from "./circuit-breaker.js";
+ class RedSkull {
+    circuiBreakerInstanceMap = new Map();
+    constructor () {
+        if (!RedSkull.instance) {
+        RedSkull.instance = this
+        }
+        // Initialize object
+        return RedSkull.instance
+    }
+
+    backoff (operation, operationName, config) {
+        if (!operation || !operationName) {
+            throw new Error("Invalid args");
+        }
+
+        // check if the instance exists
+        // if true, check the state and operate accordingly
+        // else create a new instance and push it to the map
+
+        let currInstance = this.circuiBreakerInstanceMap.get(operationName);
+        if (!currInstance) {
+            currInstance = new CircuitBreaker(config);
+            this.circuiBreakerInstanceMap.set(operationName, operation);
+        }
+        return currInstance.run(operation);
+    }
+}
+
+const instance = new RedSkull();
+Object.freeze(instance);
+export default instance;
